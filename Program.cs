780b0178b88fb17cfb102f65520c2927ecf653ac
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-21 12:27:19 +0300

    HomeWork

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Market magnit = new Market();
+        Market magnit2 = new Market();
+        Actor client1 = new OrdinaryClient("Nikolay");
+        Actor client2 = new OrdinaryClient("Sofia");
+        Actor client3 = new SpecialClient("Putin", 1);
+        TaxInspector client4 = new TaxInspector();
+        Actor client5 = new AuctionСlient("Аукционер", 2);
+        
+        magnit.AcceptToMarket(client1);
+        magnit2.AcceptToMarketForReturn(client2);
+        magnit2.AcceptToMarketForReturn(client3);
+        magnit.AcceptToMarket(client4);
+        magnit.AcceptToMarket(client5);
+        magnit.updateOrder();
+        magnit2.updateReturn();
+
+    }
+}
