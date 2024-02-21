5423a440ca4cfdb35ac079e4ac7d9a94ca0e9f99
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-21 12:28:47 +0300

    HomeWork

---
+++ classes/OrdinaryClient.cs
@@ -0,0 +1,40 @@
+public class OrdinaryClient : Actor // Дефолт клиент
+{
+
+    public OrdinaryClient(string name) : base(name) {}
+    
+    public bool isMakeOrder()
+    {
+        return base.isMakeOrder;
+    }
+
+    public bool isTakeOrder()
+    {
+        return base.isTakeOrder;
+    }
+
+    public void setMakeOrder(bool isCreated)
+    {
+        base.isMakeOrder = isCreated;
+    }
+
+    public void setTakeOrder(bool isCreated)
+    {
+        base.isTakeOrder = isCreated;
+    }
+
+    public Actor getActor()
+    {
+        return this;
+    }
+
+    public override string getName()
+    {
+        return name;
+    }
+
+    public override void setName(string name)
+    {
+        base.name = name;
+    }
+}
