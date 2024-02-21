2b3dc4f661473f6cb111b632fa09c0424298ce58
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-21 12:28:58 +0300

    HomeWork

---
+++ classes/TaxInspector.cs
@@ -0,0 +1,36 @@
+public class TaxInspector : iActorBehaviour
+{
+    private String name;
+    private bool isTakeOrder;
+    private bool isMakeOrder;
+
+    public TaxInspector()
+    {
+        this.name = "Налоговый инспектор";
+    }
+
+    bool iActorBehaviour.isTakeOrder()
+    {
+        return isTakeOrder;
+    }
+
+    bool iActorBehaviour.isMakeOrder()
+    {
+        return isMakeOrder;
+    }
+
+    public void setTakeOrder(bool isCreated)
+    {
+        isTakeOrder = isCreated;
+    }
+
+    public void setMakeOrder(bool isCreated)
+    {
+        isMakeOrder = isCreated;
+    }
+
+    public Actor getActor()
+    {
+        return new OrdinaryClient("Тайный клиент");
+    }
+}
