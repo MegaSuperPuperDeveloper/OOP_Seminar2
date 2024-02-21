37e73594c1f49e14f20949ef23685db322f3643b
Seminar's HW <krutoi.chuvak1918@gmail.com>
2024-02-21 12:28:52 +0300

    HomeWork

---
+++ classes/SpecialClient.cs
@@ -0,0 +1,46 @@
+public class SpecialClient : Actor // "Вип" клиент
+{
+
+    public SpecialClient(string name, int idVip) : base(name)
+    {
+        this.idVip = idVip;
+    }
+    
+    private int idVip;
+    
+    public bool isMakeOrder()
+    {
+        return isMakeOrder();
+    }
+
+    public bool isTakeOrder()
+    {
+        return isTakeOrder();
+    }
+
+    public void setMakeOrder(bool isCreated)
+    {
+        base.isMakeOrder = isCreated;
+    }
+
+    public void setTakeOrder(bool isCreated)
+    {
+        base.isTakeOrder = isCreated;
+    }
+
+    public Actor getActor()
+    {
+        return this;
+    }
+
+    public override string getName()
+    {
+        return name;
+    }
+
+    public override void setName(string name)
+    {
+        base.name = name;
+    }
+    
+}
